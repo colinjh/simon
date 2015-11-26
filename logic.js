e2d216bdb5f46acd09a6a7b0fe6ae3fd599a603f
Colin Harkness <Colin@Colins-MacBook-Air.local>
2015-11-26 18:08:58 +1100

    Thursday afternoon push

--- logic.js
+++ logic.js
@@ -6,118 +6,182 @@ console.log("hello world")
 // Down Green
 var compMove = [];
 var playerMove = [];
+var Rounds = 0;
+var highestScore = 0;
 
-var currentPlayer = 
 
-var tapSound = new Audio('sounds/1.ogg');
-var snapSound = new Audio('sounds/2.ogg');
-var waterSound = new Audio('sounds/3.ogg');
-var stapleSound = new Audio('sounds/4.ogg');  
+
+
+//var currentPlayer = ;
+
+var Sound1 = new Audio('sounds/1.ogg');
+var Sound2 = new Audio('sounds/2.ogg');
+var Sound3 = new Audio('sounds/3.ogg');
+var Sound4 = new Audio('sounds/4.ogg');  
 
 
 var simon = {
 		
 		redRepsonse: function() {
 			$('#red').css('opacity', 1).delay(500).queue(function(next){
-			$('#red').css("opacity", 0.5);
-			next();
-			tapSound.play();
-			})
+				$('#red').css("opacity", 0.5);
+				next();
+				Sound1.play();
+			});
 		},
 
 		blueResponse: function() {	
 			$('#blue').css('opacity', 1).delay(500).queue(function(next){
-			$('#blue').css("opacity", 0.5);
-			next();
-			snapSound.play();
-			}) 
+				$('#blue').css("opacity", 0.5);
+				next();
+				Sound2.play();
+			});
        	},
 
        	yellowResponse: function() {
        		$('#yellow').css('opacity', 1).delay(500).queue(function(next){
-			$('#yellow').css("opacity", 0.5);
-			next();
-			waterSound.play();
-			})
+				$('#yellow').css("opacity", 0.5);
+				next();
+				Sound3.play();
+			});
        	},
 
        	greenResponse: function() {
        		$('#green').css('opacity', 1).delay(500).queue(function(next){
-			$('#green').css("opacity", 0.5);
-			next();
-			stapleSound.play();
-			}) 
+				$('#green').css("opacity", 0.5);
+				next();
+				Sound4.play();
+			});
        	},	
 
 
-		keypress: function() {
-			$(document).keydown(function(e) {
-	    		if(e.which === 37) {
-	       			 //Red Left
-	       			 simon.redRepsonse();
-	       		}else if (e.which === 38) {
-	       			//Blue Up
-	       			simon.blueResponse();
-	       		}else if (e.which === 39) {
-	       			//Yellow Right
-	       			simon.yellowResponse();
+		playerTurn: function( e ) {
+
+				// IF THE COMPMOVE AND THE PLAYERMOVE ARE THE SAME LENGTH
+					// THIS ROUND IS OVEr
+
+			if ( !e ) { return; }
+			var x;
+			// $(document).keydown(function(e) {
+			
+    		if(e.which === 37) {
+       			//Red Left
+       			simon.redRepsonse();
+				x = 1;
+       		}else if (e.which === 38) {
+       			//Blue Up
+       			simon.blueResponse();
+       			x = 2;
+       		}else if (e.which === 39) {
+       			//Yellow Right
+       			simon.yellowResponse();
+       			x = 3;
 	       		}else if (e.which === 40) {	
-	       			//Green Down
-	       			simon.greenResponse();
-	       		};	 
-	    	})
+       			//Green Down
+       			simon.greenResponse();
+       			x = 4;
+       		};	 
+       		if ( x ) { 
+       			playerMove.push( x ) 
+       		};
+		    console.log('checking array lengths', playerMove.length, compMove.length);
+			if (playerMove.length === compMove.length) {
+				console.log("New turn");
+				setTimeout(function(){simon.compTurn();}, 1500);
+			};
+		    	// }
+		    	// })
 	    },
 		compTurn: function() {
-			var x = Math.ceil(Math.random()*4);
-			compMove.push(x);
+			Rounds ++;
+			$('h2.score').text('Round:' +Rounds);
+			if (compMove.toString("") !== playerMove.toString("")){
+					console.log('you lose');
+					simon.highScore();
+				}else{
+					if (compMove.length === 0) {
+						var x = Math.ceil(Math.random()*4);
+						compMove.push(x);
 
-			switch ( x ) {
-				case 1: this.redRepsonse();
-						break;
-				case 2: this.blueResponse();
-						break;
-				case 3: this.yellowResponse();
-						break;
-				case 4: this.greenResponse();
-						break;
-			}
+						switch ( x ) {
+							case 1: this.redRepsonse();
+									break;
+							case 2: this.blueResponse();
+									break;
+							case 3: this.yellowResponse();
+									break;
+							case 4: this.greenResponse();
+									break;
+						} 
+						this.playerTurn();				
+					}else{
+						var x = Math.ceil(Math.random()*4);
+						compMove.push(x);
+						this.compReplay();
+					}
+			}		
 		},
-		playerTurn: function() {
-			$(document).keydown(function(e) {
-	    		if(e.which === 37) {
-	    			console.log("left push")
-	    			var x = 1;
-	       			playerMove.push(x);
-	     
-	       		}else if (e.which === 38) {
-	       			var x = 2;
-	       			playerMove.push(x);
-	       		
-	       		}else if (e.which === 39) {
-	       			var x = 3;
-	       			playerMove.push(x);
-	       			
-	       		}else if (e.which === 40) {
-	       			var x = 4;
-	       			playerMove.push(x);	
-	       			
-	       			
-	       		};	 
+	    compReplay: function() {
+	    	var that = this;
+
+	    	for (var i = 0; i < compMove.length; i++) {
+		    	var x = compMove[i];
+
+		    	// IIFE (Immediately Invoked Function Expression - Closure)
+
+		    	(function (i, x, that) {
+
+		    		window.setTimeout(function () {
+
+				    	switch ( x ) {
+							case 1: that.redRepsonse();
+									break;
+							case 2: that.blueResponse();
+									break;
+							case 3: that.yellowResponse();
+									break;
+							case 4: that.greenResponse();
+									break;
+						}
+
+		    		}, 800 * i );
+
+		    	})(i, x, that);
+
+			}	
+			playerMove = [];		
+	    },
+	    newgame: function() {
+	    	$('#reload').on('click', function(){
+	    		console.log("new game");
+	    		compMove = [];
+	    		playerMove = [];
+	    		Rounds = 0;
+	    		simon.startGame();	
+	    	})	
+	    },
+	    highScore: function() {
+	    	if(Rounds > highestScore){
+	    		highestScore = Rounds;
+	    		console.log("high score");
+	    		$('h3').text("High Score:" +(highestScore-1));
+	    	}	
+	    },
+	    startGame: function() {
+	    	$('#start').on('click', function(){
+	    		setTimeout(function(){simon.compTurn();}, 2000);
 	    	})
 	    },
-	    winner: function() {
-	    	if (playerMove === compMove) {
-	    		playerTurn();
-	    	}
-	    }
 
 }
 $(document).ready(function() { 
-		simon.keypress();
-		simon.playerTurn();
+		simon.startGame();
+		simon.newgame();
+	$(document).keydown(function(e) {
+		simon.playerTurn( e );
+	});
+	
 
 });
-
-
 
 
